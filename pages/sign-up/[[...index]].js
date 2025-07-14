bca415acc6a3956d11e80c5fbae37408a9536d36
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2025-07-14 19:13:41 +0530

    Revert "done"
    This reverts commit 96cd6c7a9ff3b897b6c0aaf5e287a628d2590d84.

---
+++ pages/sign-up/[[...index]].js
@@ -0,0 +1,10 @@
+// pages/sign-up.js
+// import { SignUp } from '@clerk/nextjs'
+
+// export default function SignUpPage() {
+//   return (
+//     <div className="flex items-center justify-center min-h-screen bg-gray-50">
+//       <SignUp />
+//     </div>
+//   )
+// }
